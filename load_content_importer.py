438d318b3f4ef3e9b9093f4ce2134385d5d0a1e4
Chris Churas <churas.camera@gmail.com>
2019-01-11 13:21:59 -0800

    switched code to use a logger

--- load_content_importer.py
+++ load_content_importer.py
@@ -1,9 +1,14 @@
+
+import logging
 import ndex2.client as nc
 import json
 import pandas as pd
 import jsonschema
 import ndexutil.tsv.tsv2nicecx2 as t2n
 from os import path
+
+logger = logging.getLogger(__name__)
+
 
 class ContentImporter():
     def __init__(self, server, username, password, **attr):
@@ -51,10 +56,10 @@ class ContentImporter():
                 with open(load_plan_path, 'r') as lp:
                     load_plan = json.load(lp)
             except jsonschema.ValidationError as e1:
-                print("Failed to parse the loading plan: " + e1.message)
-                print('at path: ' + str(e1.absolute_path))
-                print("in block: ")
-                print(e1.instance)
+                logger.exception("Failed to parse the loading plan: " + e1.message)
+                logger.error('at path: ' + str(e1.absolute_path))
+                logger.error("in block: ")
+                logger(e1.instance)
         else:
             raise Exception('Please provide a load plan')
 
@@ -68,6 +73,7 @@ class ContentImporter():
         network = t2n.convert_pandas_to_nice_cx_with_load_plan(df, load_plan)
         network.set_name(name)
         if style_template is not None:
+            logger.debug('Applying style from network: ' + style_template)
             network.apply_template(username=self.username, password=self.password, server=self.server,
                                uuid=style_template)
 
@@ -76,14 +82,14 @@ class ContentImporter():
     def upload_network(self, re_use_metadata=True):
         network_update_key = self.update_mapping.get(self.network.get_name().upper())
         if network_update_key is not None and re_use_metadata in ['true', 'True', 'yes', True]:
-            print("Updating")
+            logger.debug("Updating")
             self.update_network_properties(network_update_key)
             message = self.network.update_to(network_update_key, self.server, self.username, self.password)
         else:
-            print("New network")
+            logger.debug("New network")
             message = self.network.upload_to(self.server, self.username, self.password)
 
-        print(message)
+        logger.info(message)
 
     def get_network_properties(self, uuid):
         net_prop_ndex = nc.Ndex2(self.server, self.username, self.password)
@@ -96,7 +102,6 @@ class ContentImporter():
             return_properties[net_prop.get('n')] = net_prop.get('v')
 
         return return_properties
-
 
     def update_network_properties(self, uuid):
         network_properties = self.get_network_properties(uuid)
